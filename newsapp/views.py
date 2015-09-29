6735aa4b4c187029e98a0533cac383a8943907ca
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2015-09-29 15:20:24 -0400

    added Foundation by Zurb frontend framework, removed twitter bootstrap and getmaterialdesign. updated the index views in views.py to use the Goose library for newspaper articles. Updated the base.html and index.html templates to be cleaner and simple.

--- newsapp/views.py
+++ newsapp/views.py
@@ -5,15 +5,6 @@ import json
 from goose import Goose
 from newsapp import app
 
-#print len(articles)
-"""
-for article in articles:
-	g = Goose()
-	news = g.extract(url=article['webUrl'])
-	print "Title: ", news.title, "\nSummary:",news.meta_description
-	#print news.cleaned_text
-	print 
-"""
 
 EDITIONS_URL = 'http://content.guardianapis.com/editions?q=%s&api-key=uzrj7hgs927dg7qx8s2bqp8q'
 SECTIONS_URL = 'http://content.guardianapis.com/sections?&api-key=uzrj7hgs927dg7qx8s2bqp8q'
@@ -21,14 +12,14 @@ SEARCH_CONTENT_URL = "http://content.guardianapis.com/search?q=%s&api-key=uzrj7h
 
 @app.route('/', methods=['GET'])
 def report_news():
-	editions_req = urllib2.Request(EDITIONS_URL % '')
-	sections_req = urllib2.Request(SECTIONS_URL)
+	#editions_req = urllib2.Request(EDITIONS_URL % '')
+	#sections_req = urllib2.Request(SECTIONS_URL)
 
-	editions_response = urllib2.urlopen(editions_req)
-	sections_response = urllib2.urlopen(sections_req)
+	#editions_response = urllib2.urlopen(editions_req)
+	#sections_response = urllib2.urlopen(sections_req)
 
-	editions_content = json.loads(editions_response.read())
-	sections_content = json.loads(sections_response.read())
+	#editions_content = json.loads(editions_response.read())
+	#sections_content = json.loads(sections_response.read())
 
 	search_bar_value = request.args.get('search_q')
 
@@ -43,14 +34,25 @@ def report_news():
 		search_response = urllib2.urlopen(search_req)
 		search_content = json.loads(search_response.read())
 
+	g = Goose()
+
 	for result in search_content['response']['results']:
+		art = g.extract(url=result['webUrl'])
+
+		result['meta_description'] = art.meta_description
+
+		print result['webUrl']
+
+		try:
+			result['image'] = art.top_image.src
+		except AttributeError, e:
+			result['image'] = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj0YzDMrnC8kqGjvTH3tQ_VpVY4HbtcpGCNcJ_tR4WdiMKvjYc'
+
 		articles.append(result)
-		#print result
-	
 
 
-	return render_template('index.html', articles=articles, 
-										editions=editions_content['response']['results'])
+	return render_template('index.html', articles=articles) 
+										#editions=editions_content['response']['results'])
 
 
 
