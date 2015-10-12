fe668d6a0a7d9a850c1f1577a7db100734f9c90d
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2015-10-12 14:29:06 -0400

    initial commit, using the Faroo Search API for news and web search. Also changed index.html structure.

--- newsapp/views.py
+++ newsapp/views.py
@@ -2,59 +2,45 @@ from flask import render_template, request, url_for
 import urllib2
 import urllib
 import json
+from datetime import datetime
 
-from goose import Goose
 from newsapp import app
 
+# GO-news-feed 1.0 => User Agent string used for Faroo Search Enginee
 
 EDITIONS_URL = 'http://content.guardianapis.com/editions?q=%s&api-key=uzrj7hgs927dg7qx8s2bqp8q'
 SECTIONS_URL = 'http://content.guardianapis.com/sections?&api-key=uzrj7hgs927dg7qx8s2bqp8q'
 SEARCH_CONTENT_URL = "http://content.guardianapis.com/search?%s&api-key=uzrj7hgs927dg7qx8s2bqp8q"
 
+TRENDING_NEWS = 'http://www.faroo.com/api?q=%s&start=1&length=10&l=en&src=news&f=json&key=he0JQxvNk2m837QQMqHIOk8E3sg_'
+NEWS_SEARCH = 'http://www.faroo.com/api?q=%s&start=1&length=20&l=en&src=news&f=json&key=he0JQxvNk2m837QQMqHIOk8E3sg_'
+
 @app.route('/', methods=['GET'])
-def report_news():
-	#editions_req = urllib2.Request(EDITIONS_URL % '')
-	#sections_req = urllib2.Request(SECTIONS_URL)
-
-	#editions_response = urllib2.urlopen(editions_req)
-	#sections_response = urllib2.urlopen(sections_req)
-
-	#editions_content = json.loads(editions_response.read())
-	#sections_content = json.loads(sections_response.read())
+def index_page():
 
 	search_bar_value = request.args.get('search_q')
-
 	articles = []
+	print search_bar_value, "QQQQQ"
 
 	if search_bar_value is not None:
-		search_bar_value = 'q=' + urllib.quote_plus(search_bar_value)
-		search_req = urllib2.Request(SEARCH_CONTENT_URL % search_bar_value)
+		search_bar_value = urllib.quote_plus(search_bar_value)
+		search_req = urllib2.Request(NEWS_SEARCH % search_bar_value)
 		search_response = urllib2.urlopen(search_req)
 		search_content = json.loads(search_response.read())
 	else:
-		search_req = urllib2.Request(SEARCH_CONTENT_URL % '')
-		search_response = urllib2.urlopen(search_req)
+		# default: get news/web results with no search term specified
+		#search_req = urllib2.Request(SEARCH_CONTENT_URL % '')
+		search_response = urllib2.urlopen(TRENDING_NEWS % search_bar_value)
 		search_content = json.loads(search_response.read())
 
-	g = Goose()
 
-	for result in search_content['response']['results']:
-		art = g.extract(url=result['webUrl'])
-
-		result['meta_description'] = art.meta_description
-
-		print result['webUrl']
-
-		try:
-			result['image'] = art.top_image.src
-		except AttributeError, e:
-			result['image'] = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj0YzDMrnC8kqGjvTH3tQ_VpVY4HbtcpGCNcJ_tR4WdiMKvjYc'
-
+	for result in search_content['results']:
+		result['date'] = datetime.fromtimestamp(int(result['date'])/1000)
+		if result['iurl'] == "":
+			print result['iurl']
+			result['iurl'] = "http://cdn.jetcharters.com/bundles/jetcharterspublic/images/image-not-found.jpg"
 		articles.append(result)
 
 
-	return render_template('index.html', articles=articles) 
+	return render_template('index.html', articles=articles)
 										#editions=editions_content['response']['results'])
-
-
-
