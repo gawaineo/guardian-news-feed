2c37b9dabecdcc2043439a80b2dd364f8865f366
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2015-10-16 14:55:16 -0400

    made port # a variable to work on heroku

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from newsapp import app
+import os
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0',debug=True, port=port)
