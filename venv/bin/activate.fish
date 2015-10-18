43c96d66d9cc3bcdb115cc79052dcf630f962364
gawaineo <gawaine.ogilvie@hotmail.com>
2015-10-18 19:33:52 -0400

    updated virtualenv folder

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gawaine/workspace/git/guardian-news-feed/venv/venv"
+set -gx VIRTUAL_ENV "/home/gawaine/workspace/git/guardian-news-feed/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
